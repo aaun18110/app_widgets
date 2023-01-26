0a5c841116349919e1938b61244d69670beabbca
aaun18110@gmail.com <aaun18110@gmail.com>
2023-01-26 20:34:38 +0500

    dev_ch#2 Compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -20,9 +20,35 @@ class Bar extends StatelessWidget {
         title: const Text("Flutter App"),
       ),
       // ignore: avoid_unnecessary_containers
-      body: Container(
-        color: Color.fromARGB(255, 9, 255, 0),
-        child: Center(child: Text("Flutter Doctor")),
+      body: Center(
+        child: Container(
+          // padding: EdgeInsets.all(10),
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Colors.blue, Colors.yellow]),
+              // shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Color.fromARGB(221, 128, 128, 128),
+                    blurRadius: 5,
+                    spreadRadius: 5,
+                    offset: Offset(3.0, 5.0))
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+                child: Text(
+              "Flutter Dev",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            )),
+          ),
+        ),
       ),
     );
   }
